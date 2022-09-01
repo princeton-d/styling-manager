42d5095c25525cd8e76cd1b63f9f8d927d775fe6
websteak <ssk0080@gmail.com>
2022-09-01 18:23:52 +0900

    firebase setup

---
+++ src/fbase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBM59RG6hJfW4jacx0T2l0K3-N59w91y10",
+  authDomain: "styling-manager-9b437.firebaseapp.com",
+  projectId: "styling-manager-9b437",
+  storageBucket: "styling-manager-9b437.appspot.com",
+  messagingSenderId: "440738531799",
+  appId: "1:440738531799:web:c856657fbd5a92adca6392"
+};
+
+const app = initializeApp(firebaseConfig);
+export const authService = getAuth();
+export default initializeApp(firebaseConfig);
