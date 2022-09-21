17df3d5c05b8f47d4c3ee68910787c0e1cf1ab60
websteak <ssk0080@gmail.com>
2022-09-21 22:47:08 +0900

    text Shadow manager 코드리펙토링

--- src/routes/textShadowManager/textShadowManager.jsx
+++ src/routes/textShadowManager/textShadowManager.jsx
@@ -4,61 +4,46 @@ import styles from './textShadowManager.module.css';
 import common from '../../common/common.module.css';
 import hexToRgb from '../../components/hexToRgb/hexToRgb';
 import TextShadowSampleList from '../../components/textShadowSampleList/textShadowSampleList';
-import { useRef } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useEffect } from 'react';
 
 const TextShadowManager = () => {
-  const shiftRightRef = useRef();
-  const shiftDownRef = useRef();
-  const blurRef = useRef();
-  const opacityRef = useRef();
-  const textShadowRef = useRef();
-  const colorPaletteRef = useRef();
-  const textColorPaletteRef = useRef();
-  const backgroundColorPaletteRef = useRef();
-  const copyTextRef = useRef();
+  const [content, setContent] = useState(1);
 
-  const [shiftRightValue, setShiftRightValue] = useState(2);
-  const [shiftDownValue, setShiftDownValue] = useState(2);
-  const [blurValue, setBlurValue] = useState(0);
-  const [opacityValue, setOpacityValue] = useState(100);
-  const [colorPaletteValue, setColorPaletteValue] = useState('#0000FF');
-  const [resultBoxColor, setResultBoxColor] = useState('#000000');
-  const [resultBoxBackgroundColor, setResultBoxBackgroundColor] =
-    useState('#999999');
-  const [rgbaValue, setRgbaValue] = useState('rgba(0, 0, 0, 1)');
+  const [inputValue, setInputValue] = useState({
+    shiftRight: 2,
+    shiftDown: 2,
+    blur: 0,
+    opacity: 100,
+    colorPalette: '#0000FF',
+    resultBoxColor: '#000000',
+    resultBoxBackgroundColor: '#999999',
+    rgba: 'rgba(0, 0, 0, 1)',
+  });
 
   const changedValue = (e) => {
-    if (e.target.name === 'shiftRight') {
-      setShiftRightValue(shiftRightRef.current.value);
-    } else if (e.target.name === 'shiftDown') {
-      setShiftDownValue(shiftDownRef.current.value);
-    } else if (e.target.name === 'blur') {
-      setBlurValue(blurRef.current.value);
-    } else if (e.target.name === 'opacity') {
-      setOpacityValue(opacityRef.current.value);
-      setRgbaValue(hexToRgb(`${colorPaletteValue}`, `${opacityValue / 100}`));
-    } else if (e.target.name === 'colorBox') {
-      setColorPaletteValue(colorPaletteRef.current.value);
-      setRgbaValue(hexToRgb(`${colorPaletteValue}`, `${opacityValue / 100}`));
+    const key = e.target.name;
+    setInputValue((prev) => ({ ...prev, [key]: e.target.value }));
+    if (
+      key === 'colorBox' ||
+      key === 'textColorBox' ||
+      key === 'backgroundColorBox'
+    ) {
+      console.log(e.target.value);
     }
   };
+
+  // console.log(colorInputValue.colorPalette);
+  // console.log(colorInputValue.resultBoxBackgroundColor);
+  // console.log(colorInputValue.resultBoxColor);
+
   useEffect(() => {
-    setRgbaValue(hexToRgb(`${colorPaletteValue}`, `${opacityValue / 100}`));
-    textShadowRef.current.style.textShadow = `${shiftRightValue}px ${shiftDownValue}px ${blurValue}px ${rgbaValue}`;
-  }, [changedValue]);
-  const changedResultBoxColor = (e) => {
-    if (e.target.name === 'textColorBox') {
-      setResultBoxColor(textColorPaletteRef.current.value);
-    } else if (e.target.name === 'backgroundColorBox') {
-      setResultBoxBackgroundColor(backgroundColorPaletteRef.current.value);
-    }
-  };
-  useEffect(() => {
-    textShadowRef.current.style.color = resultBoxColor;
-    textShadowRef.current.style.backgroundColor = resultBoxBackgroundColor;
-  }, [changedResultBoxColor]);
+    // mount, unmount
+    console.log('mound');
+    return () => {
+      console.log('unmount');
+    };
+  }, []);
   return (
     <>
       <Navigation />
@@ -66,50 +51,46 @@ const TextShadowManager = () => {
         <p className={common.title}>Text Shadow Manager</p>
         <div className={styles.stateInfoArea}>
           <div className={styles.shiftRightArea}>
-            <span>Shift Right: {shiftRightValue}</span>
+            <span>Shift Right: {inputValue.shiftRight}</span>
             <input
-              ref={shiftRightRef}
               type='range'
               name='shiftRight'
               min='-40'
               max='40'
-              value={shiftRightValue}
+              value={inputValue.shiftRight}
               onChange={changedValue}
             />
           </div>
           <div className={styles.shiftDownArea}>
-            <span>Shift Down: {shiftDownValue}</span>
+            <span>Shift Down: {inputValue.shiftDown}</span>
             <input
-              ref={shiftDownRef}
               type='range'
               name='shiftDown'
               min='-40'
               max='40'
-              value={shiftDownValue}
+              value={inputValue.shiftDown}
               onChange={changedValue}
             />
           </div>
           <div className={styles.blurArea}>
-            <span>Blur: {blurValue}</span>
+            <span>Blur: {inputValue.blur}</span>
             <input
-              ref={blurRef}
               type='range'
               name='blur'
               min='0'
               max='20'
-              value={blurValue}
+              value={inputValue.blur}
               onChange={changedValue}
             />
           </div>
           <div className={styles.opacityArea}>
-            <span>Opacity: {opacityValue / 100}</span>
+            <span>Opacity: {inputValue.opacity / 100}</span>
             <input
-              ref={opacityRef}
               type='range'
               name='opacity'
               min='0'
               max='100'
-              value={opacityValue}
+              value={inputValue.opacity}
               onChange={changedValue}
             />
           </div>
@@ -119,12 +100,11 @@ const TextShadowManager = () => {
                 text-shadow color
               </label>
               <input
-                ref={colorPaletteRef}
                 className={styles.colorBox}
                 id='colorInput'
                 type='color'
                 name='colorBox'
-                value={colorPaletteValue}
+                value={inputValue.colorPalette}
                 onChange={changedValue}
               />
             </div>
@@ -133,13 +113,12 @@ const TextShadowManager = () => {
                 text color
               </label>
               <input
-                ref={textColorPaletteRef}
                 className={styles.colorBox}
                 id='textColorInput'
                 type='color'
                 name='textColorBox'
-                value={resultBoxColor}
-                onChange={changedResultBoxColor}
+                value={inputValue.resultBoxColor}
+                onChange={changedValue}
               />
             </div>
             <div className={styles.colorBoxWrap}>
@@ -150,39 +129,42 @@ const TextShadowManager = () => {
                 background color
               </label>
               <input
-                ref={backgroundColorPaletteRef}
                 className={styles.colorBox}
                 id='backgroundColorInput'
                 type='color'
                 name='backgroundColorBox'
-                value={resultBoxBackgroundColor}
-                onChange={changedResultBoxColor}
+                value={inputValue.resultBoxBackgroundColor}
+                onChange={changedValue}
               />
             </div>
           </div>
-          <div className={styles.textShadowInfo}>
+          <div
+            className={styles.textShadowInfo}
+            onClick={(e) => {
+              e.target.innerText = 'Copy';
+              setTimeout(() => {
+                e.target.innerText = `${inputValue.shiftRight}px ${inputValue.shiftDown}px ${inputValue.blur}px ${inputValue.rgba}
+                Click To Copy`;
+              }, 1000);
+            }}
+          >
             <CopyToClipboard
-              text={`text-shadow: ${shiftRightValue}px ${shiftDownValue}px ${blurValue}px ${rgbaValue};`}
+              text={`text-shadow: ${inputValue.shiftRight}px ${inputValue.shiftDown}px ${inputValue.blur}px ${inputValue.rgba};`}
             >
-              <p
-                className={styles.textShadowCssInfo}
-                onClick={() => {
-                  copyTextRef.current.innerText = 'Copy';
-                  setTimeout(() => {
-                    copyTextRef.current.innerText = 'Click To Copy';
-                  }, 1000);
-                }}
-              >
-                {shiftRightValue}px {shiftDownValue}
-                px {blurValue}px {rgbaValue}
+              <p className={styles.textShadowCssInfo}>
+                {inputValue.shiftRight}px {inputValue.shiftDown}
+                px {inputValue.blur}px {inputValue.rgba}
                 <br />
-                <span ref={copyTextRef}>Click To Copy</span>
+                Click To Copy
               </p>
             </CopyToClipboard>
           </div>
-          <div ref={textShadowRef} className={styles.resultText}>
+          <div className={styles.resultText}>
             <p>Result Text</p>
           </div>
+          <button onClick={() => setContent((prev) => prev + 1)}>
+            {content}
+          </button>
         </div>
         <TextShadowSampleList />
       </section>
